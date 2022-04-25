e27d9bf1bb0860eb6a938b6e656f204c7a65147e
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-04-24 23:51:25 -0400

    created a function create password

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.8
+from password import Password
+
+def create_password(user_name, user_password):
+    '''
+    Function to create a new contact
+    '''
+    new_password = Password(user_name, user_password)
+    return new_password
