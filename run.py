5af9ef87eec3cea7f9c93158ee02a7d30229f86f
Valarie Chelagat <valarie.chelagat@student.moringaschool.com>
2022-04-24 23:58:10 -0400

    closed my code

--- run.py
+++ run.py
@@ -102,3 +102,7 @@ def main():
                             break
                     else:
                             print("I really didn't get that. Please use the short codes")
+
+if __name__ == '__main__':
+
+ main()
